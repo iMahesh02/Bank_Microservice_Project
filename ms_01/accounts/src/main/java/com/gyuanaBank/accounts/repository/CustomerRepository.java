a2806cd38ddbe2e6d0587ee0b734ac6f478d832d
iMahesh02 <manuwarghane999@yahoo.com>
2024-06-29 14:48:57 +0530

    Foundation for the Microservice 01

---
+++ ms_01/accounts/src/main/java/com/gyuanaBank/accounts/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.gyuanaBank.accounts.repository;
+
+import com.gyuanaBank.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
