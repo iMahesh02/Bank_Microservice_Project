0a9a2150cb068b34f4802b2e827d81610fa67563
iMahesh02 <manuwarghane999@yahoo.com>
2024-06-30 16:24:44 +0530

    Added update API, This api can be used to update the customer details except Account Number.

--- ms_01/accounts/src/main/java/com/gyuanaBank/accounts/service/IAccountsService.java
+++ ms_01/accounts/src/main/java/com/gyuanaBank/accounts/service/IAccountsService.java
@@ -1,10 +1,13 @@
 package com.gyuanaBank.accounts.service;
 
 import com.gyuanaBank.accounts.dto.CustomerDto;
+import com.gyuanaBank.accounts.entity.Customer;
 
 public interface IAccountsService {
 
     void createAccount(CustomerDto customerDto);
 
     CustomerDto fetchAccount(String mobileNumber);
+
+    boolean updateAccount(CustomerDto customerDto);
 }
