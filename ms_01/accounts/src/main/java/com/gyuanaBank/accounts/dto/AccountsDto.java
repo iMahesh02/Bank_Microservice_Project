9883eec1a2b0b815fabad7d1e1486baff20b4dd7
iMahesh02 <manuwarghane999@yahoo.com>
2024-07-06 17:13:02 +0530

    Added Internal Exception Handler and Input Validation

--- ms_01/accounts/src/main/java/com/gyuanaBank/accounts/dto/AccountsDto.java
+++ ms_01/accounts/src/main/java/com/gyuanaBank/accounts/dto/AccountsDto.java
@@ -1,5 +1,7 @@
 package com.gyuanaBank.accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Setter;
@@ -8,10 +10,14 @@ import lombok.Setter;
 @Setter
 public class AccountsDto {
 
+    @NotEmpty
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Account number must be 10 digits")
     private Long accountNumber;
 
+    @NotEmpty(message = "Account type can not be null or empty")
     private String accountType;
 
+    @NotEmpty(message = "Branch address can not be a null or empty")
     private String branchAddress;
 
     public void setAccountNumber(Long accountNumber) {
